f31b1ea107fbc00003383b0874066c19afc276b2
Saule Guzyte <saule.guzyte@tesco.com>
2019-12-12 18:07:20 +0000

    Change in balance data type for better accuracy

--- database/MembershipCard/create_db/020_Cards.sql
+++ database/MembershipCard/create_db/020_Cards.sql
@@ -1,13 +1,13 @@
 USE [Membership_Card]
 GO
-CREATE TABLE [dbo].[testing](
-                                [user_id] [int] IDENTITY(1,1) PRIMARY KEY,
-                                [employee_id] [varchar](20) UNIQUE NOT NULL,
-                                [first_name] [varchar](50) NOT NULL,
-                                [second_name] [varchar](50) NOT NULL,
-                                [mobile_number] [varchar](22) NOT NULL,
-                                [card_id] [varchar](16) UNIQUE NOT NULL,
-                                [pin] [varchar](4) NULL,
-                                [balance] [money]
+CREATE TABLE [dbo].[Cards](
+                                 [user_id] [int] IDENTITY(1,1) PRIMARY KEY,
+                                 [employee_id] [varchar](20) UNIQUE NOT NULL,
+                                 [first_name] [varchar](50) NOT NULL,
+                                 [second_name] [varchar](50) NOT NULL,
+                                 [mobile_number] [varchar](22) NOT NULL,
+                                 [card_id] [varchar](16) UNIQUE NOT NULL,
+                                 [pin] [varchar](4) NULL,
+                                 [balance] [decimal](10,2)
 )
 GO
