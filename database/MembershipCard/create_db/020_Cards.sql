ab807b5bd2f0a0fd59639487df420d10d614a913
Saule Guzyte <saule.guzyte@tesco.com>
2019-12-12 09:11:29 +0000

    Card table updated to include balance column

--- database/MembershipCard/create_db/020_Cards.sql
+++ database/MembershipCard/create_db/020_Cards.sql
@@ -1,16 +1,13 @@
 USE [Membership_Card]
 GO
-CREATE TABLE [dbo].[Card](
-                             [user_id] [int] IDENTITY(1,1) PRIMARY KEY,
-                             [employee_id] [varchar](20) UNIQUE NOT NULL,
-                             [first_name] [varchar](50) NOT NULL,
-                             [second_name] [varchar](50) NOT NULL,
-                             [mobile_number] [varchar](22) NOT NULL,
-                             [card_id] [varchar](16) UNIQUE NOT NULL,
-                             [pin] [varchar](4) NULL
+CREATE TABLE [dbo].[testing](
+                                [user_id] [int] IDENTITY(1,1) PRIMARY KEY,
+                                [employee_id] [varchar](20) UNIQUE NOT NULL,
+                                [first_name] [varchar](50) NOT NULL,
+                                [second_name] [varchar](50) NOT NULL,
+                                [mobile_number] [varchar](22) NOT NULL,
+                                [card_id] [varchar](16) UNIQUE NOT NULL,
+                                [pin] [varchar](4) NULL,
+                                [balance] [money]
 )
-GO
-
-
-USE [Membership_Card]
 GO
