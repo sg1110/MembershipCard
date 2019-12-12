8f9c9506a7bc47bc7c28dcc3e86efa8532a6e8d5
Saule Guzyte <saule.guzyte@tesco.com>
2019-12-12 17:19:06 +0000

    Unhappy Login Path Tested

--- tests/MembershipCardSystem.IntegrationTests/LogIn/WhenValidCardIdAndInvalidPinIsProvided.cs
+++ tests/MembershipCardSystem.IntegrationTests/LogIn/WhenValidCardIdAndInvalidPinIsProvided.cs
@@ -1,7 +1,55 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
 namespace MembershipCardSystem.IntegrationTests.LogIn
 {
-    public class WhenValidCardIdAndInvalidPinIsProvided
+    public class WhenValidCardIdAndInvalidPinIsProvided : TestBase
     {
+        private readonly IDbConnection _connection;
+
+        private const string ConnectionString = "Server=localhost,1433;Database=Membership_Card;User Id=sa;Password=reallyStrongPwd123";
+
         
+        public WhenValidCardIdAndInvalidPinIsProvided(WebApplicationFactory<Startup> factory) : base(factory)
+        {
+            _connection = new SqlConnection(ConnectionString);
+            ClearTestData();
+            SetupTestUserWithPin();
+        }
+        
+        private void SetupTestUserWithPin()
+        {
+            var sql = "INSERT INTO [dbo].[Card] (employee_id, first_name, second_name, mobile_number, card_id, pin) VALUES ('TestID8', 'TEST','mctest', '1234567890', '1234cc7890123456', '1234' )";
+
+            _connection.Execute(sql);
+        }
+        
+        private void ClearTestData()
+        {
+            _connection.Execute("DELETE FROM [dbo].[Card] WHERE employee_id in ('TestID8')");
+        }
+
+        [Fact]
+        public async void Will_return_no_content()
+        { 
+            var client = Factory.CreateClient();
+          
+            var response = await client.PostAsync("membershipcard/login",
+                new StringContent(
+                    "{\"CardId\": \"1234cc7890123456\"," +
+                    "\"CardPin\": \"4444\",}",
+                    Encoding.UTF8, "application/json"));;
+            
+            Assert.Equal((HttpStatusCode) StatusCodes.Status401Unauthorized, response.StatusCode);
+
+        }
     }
 }
+
